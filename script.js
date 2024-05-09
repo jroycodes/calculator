f86809be018981e04f9a7d54fe50d9e60e610ba1
Jonathan Roy <jonaisroy@gmail.com>
2024-05-09 14:36:19 +0300

    operate function

--- script.js
+++ script.js
@@ -3,21 +3,41 @@
 // CALCULATOR FUNCTIONS
 
 // Addition
-const addition = function (num1, num2) {
+function addition(num1, num2) {
   return num1 + num2;
-};
+}
 
 // Subtraction
-const subtraction = function (num1, num2) {
+function subtraction(num1, num2) {
   return num1 - num2;
-};
+}
 
 // Multiplication
-const multiply = function (num1, num2) {
+function multiply(num1, num2) {
   return num1 * num2;
-};
+}
 
 // Division
-const divide = function (num1, num2) {
+function divide(num1, num2) {
   return num1 / num2;
-};
+}
+
+// Operation variables
+let firstNum;
+let operator;
+let secondNum;
+
+//  function operate that takes an operator and 2 numbers and calls upper functions
+
+function operate(operator, firstNum, secondNum) {
+  if (operator === "+") {
+    return addition(firstNum, secondNum);
+  } else if (operator === "-") {
+    return subtraction(firstNum, secondNum);
+  } else if (operator === "*") {
+    return multiply(firstNum, secondNum);
+  } else if (operator === "/") {
+    divide(firstNum, secondNum);
+  } else return "Invalid operator";
+}
+console.log(operate("+", 12, 13));
