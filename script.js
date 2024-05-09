1360d173ea6f5613176630fd8d48e6b14d3aab0a
Jonathan Roy <jonaisroy@gmail.com>
2024-05-09 14:10:17 +0300

    addition and subtraction functions

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// CALCULATOR FUNCTIONS
+
+// Addition
+const addition = function (num1, num2) {
+  return num1 + num2;
+};
+
+// Subtraction
+const subtraction = function (num1, num2) {
+  return num1 - num2;
+};
