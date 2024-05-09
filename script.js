cb1ff12701c8aed3e17e4047bea25a379fa67730
Jonathan Roy <jonaisroy@gmail.com>
2024-05-09 14:13:55 +0300

    multiplication and division functions

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ const addition = function (num1, num2) {
 const subtraction = function (num1, num2) {
   return num1 - num2;
 };
+
+// Multiplication
+const multiply = function (num1, num2) {
+  return num1 * num2;
+};
+
+// Division
+const divide = function (num1, num2) {
+  return num1 / num2;
+};
