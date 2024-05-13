3225ce1ecd89587d478e629442dcfe9c4fd32285
Jonathan Roy <jonaisroy@gmail.com>
2024-05-13 13:57:32 +0300

    functions to listen and store values from event

--- script.js
+++ script.js
@@ -1,47 +1,23 @@
 "use strict";
 
-// CALCULATOR FUNCTIONS
+let firstNum = "";
+let currentOperator = "";
+let secondNum = "";
 
-// Addition
-function addition(num1, num2) {
-  return num1 + num2;
+// stores values in first and second num variables depending on click event 
+function buttonClick(event) {
+  const clickedNumber = event.target.textContent;
+  if (!currentOperator) {
+    firstNum += clickedNumber;
+    updateDisplay(firstNum);
+  } else {
+    secondNum += clickedNumber;
+    updateDisplay(secondNum);
+  }
 }
 
-// Subtraction
-function subtraction(num1, num2) {
-  return num1 - num2;
-}
-
-// Multiplication
-function multiply(num1, num2) {
-  return num1 * num2;
-}
-
-// Division
-function divide(num1, num2) {
-  return num1 / num2;
-}
-
-// Operation variables
-let firstNum;
-let operator;
-let secondNum;
-
-//  function operate that takes an operator and 2 numbers and calls upper functions
-
-function operate(operator, firstNum, secondNum) {
-  if (operator === "+") {
-    return addition(firstNum, secondNum);
-  } else if (operator === "-") {
-    return subtraction(firstNum, secondNum);
-  } else if (operator === "*") {
-    return multiply(firstNum, secondNum);
-  } else if (operator === "/") {
-    if (secondNum !== 0) {
-      return divide(firstNum, secondNum);
-    } else {
-      return "Cannot divide by zero";
-    }
-  } else return "Invalid operator";
-}
-console.log(operate("+", 12, 13));
+// listen for button clicks 
+const numberButtons = document.querySelectorAll(".numbers");
+numberButtons.forEach((button) => {
+  button.addEventListener("click", buttonClick);
+});
