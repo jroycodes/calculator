db95b99a3d6f72d3be82159c7ef7e4022b4915d0
Jonathan Roy <jonaisroy@gmail.com>
2024-05-13 14:06:11 +0300

    operations function

--- script.js
+++ script.js
@@ -35,3 +35,32 @@ function operatorButtonClick(event) {
 document.querySelectorAll(".operators").forEach((button) => {
   button.addEventListener("click", operatorButtonClick);
 });
+
+// carry out operations
+function operate() {
+  const num1 = parseFloat(firstNum);
+  const num2 = parseFloat(secondNum);
+  let result;
+  switch (currentOperator) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      if (num2 !== 0) {
+        result = num1 / num2;
+      } else {
+        result = "Error: Division by zero";
+      }
+      break;
+    default:
+      result = "Invalid operator";
+  }
+
+  updateDisplay(result);
+}
